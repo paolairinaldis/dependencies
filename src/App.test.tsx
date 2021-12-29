13be3cd3241be8865a5661901f1772c2f94d396d
Paola Rinaldi <prinaldi@onalap1218.corp.onapsis.com>
2021-12-29 14:56:40 -0300

    dependencies app App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import DependenciesContainer from './dependencies-view/dependencies.container';
+
+// TODO: LOAD TO ENZYME/JEST CONFIG FILE
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+describe('DependenciesContainer Test', () => {
+    it('should render DependenciesContainer and components', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toHaveLength(1);
+        expect(wrapper.find(DependenciesContainer)).toHaveLength(1);
+    });
 });
